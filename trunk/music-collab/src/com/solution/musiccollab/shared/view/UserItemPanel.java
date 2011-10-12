baead85836a1804896ee949229d3a159c9de53c1
cooooooop@gmail.com <cooooooop@gmail.com@adfb7e81-2c3e-240e-cc96-548b542f2982>
2011-10-12 05:34:42 +0000

    added last login to user item panel

--- trunk/music-collab/src/com/solution/musiccollab/shared/view/UserItemPanel.java
+++ trunk/music-collab/src/com/solution/musiccollab/shared/view/UserItemPanel.java
@@ -1,24 +1,27 @@
 package com.solution.musiccollab.shared.view;
 
+import java.util.Date;
+
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiTemplate;
-import com.google.gwt.user.client.Command;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.Widget;
 import com.solution.musiccollab.shared.event.NavigationEvent;
 import com.solution.musiccollab.shared.event.NavigationEventHandler;
-import com.solution.musiccollab.shared.model.Model;
 import com.solution.musiccollab.shared.value.UserDAO;
 
 public class UserItemPanel extends Composite {
 	
 	@UiField
 	Label nameLabel;
+	
+	@UiField
+	Label loginLabel;
 	
 	private UserDAO data;
 	
@@ -32,7 +35,9 @@ public class UserItemPanel extends Composite {
 
 	public void setupPanel(final UsersList parent, final UserDAO userDAO) {
 		data = userDAO;
-		nameLabel.setText(userDAO.getNickname());
+		nameLabel.setText(data.getNickname());
+		
+		loginLabel.setText(getLoginText());
 		
 		nameLabel.addClickHandler(new ClickHandler() {
 			
@@ -43,6 +48,35 @@ public class UserItemPanel extends Composite {
 		        }
 			}
 		});
+	}
+	
+	private String getLoginText() {
+		Date today = new Date();
+		int days = getDaysDiff(today, data.getLastLogin());
+		int hours = getHoursDiff(today, data.getLastLogin());
+		int mins = getMinsDiff(today, data.getLastLogin());
+		
+		if(days > 0)
+			return "Last seen: " + days + " days ago";
+		else if(hours > 0)
+			return "Last seen: " + hours + " hours ago";
+		else if(mins > 0)
+			return "Last seen: " + mins + " minutes ago";
+		
+		return "Online now";
+	}
+	
+	private int getDaysDiff(Date date1, Date date2) {
+		long temp = Math.abs(date1.getTime() - date2.getTime());
+		return Math.abs(((int)date1.getTime() - (int)date2.getTime()) / (1000 * 60 * 60 * 24));
+	}
+	
+	private int getHoursDiff(Date date1, Date date2) {
+		return Math.abs(((int)date1.getTime() - (int)date2.getTime()) / (1000 * 60 * 60));
+	}
+	
+	private int getMinsDiff(Date date1, Date date2) {
+		return Math.abs(((int)date1.getTime() - (int)date2.getTime()) / (1000 * 60));
 	}
 	
 }
