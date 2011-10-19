3529533e7823f59eb422543bb606696e1fe6b8d0
cooooooop <cooooooop@adfb7e81-2c3e-240e-cc96-548b542f2982>
2011-10-19 01:40:46 +0000

    added audio test project (ServiceProject)

--- trunk/ServiceProject/src/com/solution/musicservice/AudioUtil.java
+++ trunk/ServiceProject/src/com/solution/musicservice/AudioUtil.java
@@ -1,19 +1,64 @@
 package com.solution.musicservice;
 
+import java.io.BufferedInputStream;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.sound.sampled.AudioFileFormat;
+import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
+
+//import org.tritonus.sampled.convert.vorbis.VorbisFormatConversionProvider.EncodedVorbisAudioInputStream;
 
 import com.solution.musicservice.audio.MixingFloatAudioInputStream;
 import com.solution.musicservice.audio.SequenceAudioInputStream;
 
 public class AudioUtil {
+	
+	public static byte[] decodeOgg(byte[] sample) {
+//		try {
+//			ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(sample);
+//			AudioInputStream source = new AudioInputStream(byteArrayInputStream, , length)
+//			AudioInputStream pcm = AudioSystem.getAudioInputStream(AudioFormat.Encoding.PCM_SIGNED, source);
+//			AudioInputStream ulaw = AudioSystem.getAudioInputStream(AudioFormat.Encoding.ULAW, pcm);
+//			File tempFile = File.createTempFile("wav", "tmp");
+//			AudioSystem.write(ulaw, AudioFileFormat.Type.WAVE, tempFile);
+//
+//			byte[] bytes = Main.getBytesFromFile(tempFile);
+//			tempFile.delete();
+//			return bytes;
+//		}
+//		catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//		
+		return null;
+	}
+	
+	public static byte[] decodeMp3(byte[] sample) {
+		try {
+			ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(sample);
+			AudioInputStream source = AudioSystem.getAudioInputStream(byteArrayInputStream);
+			AudioInputStream pcm = AudioSystem.getAudioInputStream(AudioFormat.Encoding.PCM_SIGNED, source);
+			AudioInputStream ulaw = AudioSystem.getAudioInputStream(AudioFormat.Encoding.ULAW, pcm);
+			File tempFile = File.createTempFile("wav", "tmp");
+			AudioSystem.write(ulaw, AudioFileFormat.Type.WAVE, tempFile);
+
+			byte[] bytes = Main.getBytesFromFile(tempFile);
+			tempFile.delete();
+			return bytes;
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
 	
 	public static float decibel2linear(float decibels) {
 		return (float) Math.pow(10.0, decibels / 20.0);
