1674d1aa7b67f2be6f6575a08dabd4f8c854440e
cooooooop@gmail.com <cooooooop@gmail.com@adfb7e81-2c3e-240e-cc96-548b542f2982>
2012-02-07 03:36:50 +0000

    added mix page

---
+++ trunk/music-collab/src/com/solution/musiccollab/shared/view/MixPage.java
@@ -0,0 +1,143 @@
+package com.solution.musiccollab.shared.view;
+
+import java.util.ArrayList;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiTemplate;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RichTextArea;
+import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Widget;
+import com.solution.musiccollab.shared.event.DAOEvent;
+import com.solution.musiccollab.shared.event.DAOEventHandler;
+import com.solution.musiccollab.shared.event.NavigationEvent;
+import com.solution.musiccollab.shared.event.NavigationEventHandler;
+import com.solution.musiccollab.shared.model.Model;
+import com.solution.musiccollab.shared.value.AudioFileDAO;
+import com.solution.musiccollab.shared.value.MixDAO;
+import com.solution.musiccollab.shared.value.UserDAO;
+
+public class MixPage extends Composite implements IUpdatable, IDAOEditor {
+
+	@UiField
+	AudioFilesList audioFilesList;
+	
+	@UiField
+	Label userNameLabel;
+	
+	@UiField
+	Label titleLabel;
+	
+	@UiField
+	Label backLabel;
+	
+	@UiField
+	VerticalPanel richTextPanel;
+	
+	@UiField
+	Button saveButton;
+	
+	private ArrayList<NavigationEventHandler> handlers = new ArrayList<NavigationEventHandler>();
+	private ArrayList<DAOEventHandler> daoHandlers = new ArrayList<DAOEventHandler>();
+	private MixDAO mixDAO;
+	private RichTextArea richTextArea = new RichTextArea();
+	private RichTextToolbar richTextToolbar = new RichTextToolbar(richTextArea);
+	
+	@UiTemplate("uibinder/MixPage.ui.xml")
+	interface MyUiBinder extends UiBinder<Widget, MixPage> { }
+	private static final MyUiBinder uiBinder = GWT.create(MyUiBinder.class);
+
+	public MixPage() {
+		initWidget(uiBinder.createAndBindUi(this));
+		
+		backLabel.addClickHandler(new ClickHandler() {
+			
+			@Override
+			public void onClick(ClickEvent event) {
+				for(NavigationEventHandler handler : handlers) {
+					handler.onHomeNavigation(new NavigationEvent(Model.currentUser));
+		        }
+			}
+		});
+		
+		userNameLabel.addClickHandler(new ClickHandler() {
+			
+			@Override
+			public void onClick(ClickEvent event) {
+				for(NavigationEventHandler handler : handlers) {
+					handler.onMemberPageNavigation(new NavigationEvent(MixPage.this.mixDAO.getOwnerUserDAO()));
+		        }
+			}
+		});
+		
+		richTextArea.setWidth("100%");
+		richTextPanel.add(richTextToolbar);
+		richTextPanel.add(richTextArea);		
+		richTextPanel.setHeight("100%");
+	}
+	
+	@Override
+	public void update() {
+		audioFilesList.setCaptionText("Audio Files");
+		audioFilesList.setVisible(audioFilesList.getSize() > 0);
+	}
+	
+	public void setMixDAO(MixDAO mixDAO) {
+		this.mixDAO = mixDAO;
+		
+		this.userNameLabel.setText(mixDAO.getOwnerUserDAO().getNickname());
+		this.titleLabel.setText(mixDAO.getMixName());
+		
+		this.richTextArea.setHTML(mixDAO.getUserContent());
+		
+		saveButton.addClickHandler(new ClickHandler() {
+			
+			@Override
+			public void onClick(ClickEvent event) {
+				MixPage.this.mixDAO.setUserContent(richTextArea.getHTML());
+				for(DAOEventHandler handler : daoHandlers) {
+					handler.onSaveMix(new DAOEvent(MixPage.this.mixDAO, MixPage.this));
+		        }
+			}
+		});
+	}
+	
+	public AudioFilesList getAudioFilesList() {
+		return audioFilesList;
+	}
+	
+	public void addNavigationEventHandler(NavigationEventHandler handler) {
+		handlers.add(handler);
+    }
+
+    public void removeNavigationEventHandler(NavigationEventHandler handler) {
+    	handlers.remove(handler);
+    }
+    
+    public void addDAOEventHandler(DAOEventHandler handler) {
+		daoHandlers.add(handler);
+    }
+
+    public void removeDAOEventHandler(DAOEventHandler handler) {
+    	daoHandlers.remove(handler);
+    }
+
+	@Override
+	public void removeDAO(Object dao) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateDAO(Object dao) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
