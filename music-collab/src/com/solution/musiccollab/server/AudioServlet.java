6ed5c8a0c367c9c0ff96b1fa502c043dc7a917ad
cooooooop@gmail.com <cooooooop@gmail.com@adfb7e81-2c3e-240e-cc96-548b542f2982>
2012-06-25 03:07:47 +0000

    added json servlet to get users/audio

---
+++ music-collab/src/com/solution/musiccollab/server/AudioServlet.java
@@ -0,0 +1,48 @@
+package com.solution.musiccollab.server;
+
+import java.io.IOException;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.googlecode.objectify.Query;
+import com.solution.musiccollab.shared.value.AudioFileDAO;
+import com.solution.musiccollab.shared.value.UserDAO;
+
+public class AudioServlet extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+    private DAO dao = new DAO();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		dao = new DAO();
+		
+		int limit;
+		String order = req.getParameter("order");
+		Query<AudioFileDAO> query = dao.ofy().query(AudioFileDAO.class);
+		try {
+			limit = Integer.parseInt(req.getParameter("limit"));
+			query = query.limit(limit);
+		}
+		catch (NumberFormatException e) {
+			//ignore
+		}
+		
+		if(order != null)
+			query = query.order(order);
+		
+		
+    	JSONSerializer.audioListSerialize(query.list(), resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    	dao = new DAO();
+		List<UserDAO> users = dao.ofy().query(UserDAO.class).order("lastLogin").list();
+    	JSONSerializer.userListSerialize(users, resp);
+        
+    }
+}
